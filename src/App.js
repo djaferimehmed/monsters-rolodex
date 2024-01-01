00d1368f078461dbce6cf9486cde9034ce39234d
Mehmed Djaferi <mdm@mdmBook.mdm.local>
2024-01-01 13:18:03 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -7,14 +7,53 @@ class App extends Component {
   constructor() {
     super(); {
       this.state = {
-        name: { firstname: 'Mehmed', lastname: 'Top'},
-        company: 'DG'
+        monsters: [],
+        searchField: ''
       }
     }
   }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => 
+      this.setState(
+        () => {
+          return {monsters: users}
+        },
+        () => {
+          console.log(this.state)
+        } 
+    ))
+  }
+
+  onSearchChange = (event) => {
+    console.log(event.target.value)
+    const searchField = event.target.value.toLocaleLowerCase()
+    this.setState(() => {
+      return {searchField}
+    })
+  }
+
   render() {
+
+    const { monsters, searchField } = this.state
+    const { onSearchChange } = this
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField)
+    })
     return (
-      <div className="App"></div>
+      <div className="App">
+        <input 
+          className='search-box' 
+          type='search' 
+          placeholder='search monsters' 
+          onChange={onSearchChange}/>
+        {filteredMonsters.map((monster) => {
+            return <h1 key={monster.id}>{monster.name}</h1>;
+          })}
+      </div>
     );
   }
 
